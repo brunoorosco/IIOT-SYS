3aa234b20bad685e9b2e2d04e5bca27ed62b8906
brunoorosco <brunoorosco@gmail.com>
2021-09-29 22:47:41 -0300

    ajustes css

--- front/src/App.js
+++ front/src/App.js
@@ -1,49 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import socketIOClient from 'socket.io-client'
 
 import './App.css';
 
 const socket = socketIOClient("http://localhost:3005")
 
-class App extends Component {
-  
+function App(props) {
 
-  constructor() {
-    super()
+  const [infoReb, setInfo] = useState("")
 
-    this.state = {
-      serverURL: "http://localhost:3005",
-      informationReceived: ' Nothing yet! you should click on the button'
-    }
+  socket.on('infoEvent', (receivedInfo) => {
+    setInfo(receivedInfo)
+  })
 
-     //  const socket = socketIOClient(this.state.serverURL)
-    socket.on('infoEvent', (receivedInfo) => {
-      this.setState({
-        informationReceived: receivedInfo
-      })
-    })
-  }
-
-  emitInfoToAll = () => {
-  
-    //  const socket = socketIOClient(this.state.serverURL)
+  function emitInfoToAll() {
+    // const socket = socketIOClient(this.state.serverURL)
     socket.emit('infoEvent', 'hello realtime')
   }
 
-  render() {
-    return (
+
+  return (
+    <>
       <div className="App">
         <div>
-          <button onClick={() => this.emitInfoToAll()}>Send information to all connected clients.</button>
+          <button onClick={() => emitInfoToAll()}>Send information to all connected clients.</button>
         </div>
         <br />
-        {
-          this.state.informationReceived
-        }
+        <div className="box">
+          <div className="titulo">
+            <p>TEMPO:</p>
+          </div>
 
+          <div className="sub-titulo">
+            {infoReb}
+          </div>
+        </div>
       </div>
-    );
-  }
+    </>
+  )
+
 }
 
 export default App;
